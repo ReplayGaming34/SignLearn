dae77708a8f94bc116b0ff5a5d6c2e2d8eae914b
GraysonB <replaygaming34@gmail.com>
2026-03-05 16:49:26 -0500

    Execute file

---
+++ src/mustRun.py
@@ -0,0 +1,33 @@
+from app import App
+from model_factory import model_factory
+from processing import start_processing
+from train import start_training
+
+
+def train_and_run():
+    print("starting processing")
+    start_processing()
+    print("processing complete")
+    print("creating model factory")
+    mf = model_factory()
+    print("creating model")
+    model = mf.create_model()
+    print("starting training")
+    start_training(model)
+
+    print("starting app")
+    app = App()
+    app.start()
+
+
+def run_app():
+    app = App()
+    app.start()
+
+
+if __name__ == "__main__":
+    # Uncomment the line below to run the full pipeline (processing + training + app)
+    # train_and_run()
+
+    # If you only want to run the app (after processing and training are done), use this:
+    run_app()
